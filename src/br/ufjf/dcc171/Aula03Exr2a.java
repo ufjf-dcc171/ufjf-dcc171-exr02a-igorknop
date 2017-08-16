f1d805f19f389fbc8b8499b410c14cafc8d5e4c9
Igor Knop <igorknop@gmail.com>
2017-08-15 22:26:59 -0300

    Inicia uma solução inicial

---
+++ src/br/ufjf/dcc171/Aula03Exr2a.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula03Exr2a {
+
+    public static void main(String[] args) {
+        JanelaSoma janela = new JanelaSoma();
+        janela.setSize(300,200);
+        janela.setLocationRelativeTo(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+    }
+    
+}
